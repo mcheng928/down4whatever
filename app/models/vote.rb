38f88f2cc4acf6a8e5a17addca60a15dc251a24b
Raghu Betina <raghu@firstdraft.com>
2016-12-08 03:48:48 +0000

    Validate presence of votes.choice_id

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,6 +5,8 @@ class Vote < ApplicationRecord
 
   # Validations
 
+  validates :choice_id, :presence => true
+
   validates :guest_id, :uniqueness => { :scope => [:choice_id] }
 
   validates :guest_id, :presence => true
