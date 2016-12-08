7579e3ebae6f4011f245b701cbc649eac779b5cb
Raghu Betina <raghu@firstdraft.com>
2016-12-08 03:48:48 +0000

    Validate presence of users.phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :phone, :presence => true
+
   validates :phone, :length => { :minimum => 7, :maximum => 7 }
 
   # Include default devise modules. Others available are:
